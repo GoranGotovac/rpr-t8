60d08ee532250d4474ba6d3753e9beb9a9503285
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-12-11 15:55:29 +0100

    dodano disable i Zaustavi

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -1,4 +1,59 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+
 public class Controller {
+    public TextField tekst;
+    public ListView<String> lista;
+    private ObservableList<String> observableList = FXCollections.synchronizedObservableList(FXCollections.observableArrayList());
+    @FXML
+    private Button btnZaustavi;
+    @FXML
+    private Button btnTrazi;
+    public boolean isPressed = false;
+    public Thread root;
+    public void fileSearch(String pocetak, String rijec) {
+        File root = new File(pocetak);
+        File[] list = root.listFiles();
+        if (list == null) {
+            return;
+        }
+        for (File f : list) {
+            if (f.isDirectory()) {
+                fileSearch(f.getAbsolutePath(), rijec);
+            } else {
+                if (f.getAbsoluteFile().toString().contains(rijec)) {
+                    Platform.runLater(() -> observableList.add(f.getAbsoluteFile().toString()));
+                    lista.getItems().add(""+f.getAbsoluteFile());
+
+                }
+            }
+        }
+    }
+    public void trazi(ActionEvent actionEvent) {
+        isPressed=true;
+        btnTrazi.setDisable(true);
+        btnZaustavi.setDisable(false);
+            root =  new Thread(() -> {
+                if (!lista.getItems().isEmpty()) {
+                    Platform.runLater(() -> lista.getItems().clear());
+                }
+                fileSearch("C:\\Users", tekst.getText());
+                Platform.runLater(() -> lista.setItems(observableList));
+            });
+            root.start();
+
+    }
+    public void zaustavi(ActionEvent actionEvent) {
+        root.stop();
+    }
 }
--- src/ba/unsa/etf/rpr/tutorijal8/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -6,15 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
